7f6ec7b20fafc48d83c85945936c3f21ba6435e4
lowwzin <164194786+lowwzin@users.noreply.github.com>
2024-03-20 17:58:53 +0400

    Program.cs

---
+++ BraveBypass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BraveBypass
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>s
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new bypass());
+        }
+    }
+}
