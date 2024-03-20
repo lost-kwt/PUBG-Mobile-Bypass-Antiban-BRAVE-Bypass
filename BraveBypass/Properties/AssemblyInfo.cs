ee137d3f5f70b07afbf28d76c681f17aaf7a25ae
lowwzin <164194786+lowwzin@users.noreply.github.com>
2024-03-20 18:13:04 +0400

    Brave Bypass

--- BraveBypass/Properties/AssemblyInfo.cs
+++ BraveBypass/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("pubgm bypass source")]
+[assembly: AssemblyTitle("Brave Bypass")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("pubgm bypass source")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyProduct("Brave Bypass")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
